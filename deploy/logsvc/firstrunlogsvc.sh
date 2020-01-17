bb8c93b69cd2774b0a0b975e2ad04cee0ced7840
wuzhangwei <wuzhangwei@ttop9.com>
2020-01-17 15:10:02 +0800

    <fix>(<deploy>): 更新logsvc启动脚本

--- deploy/logsvc/firstrunlogsvc.sh
+++ deploy/logsvc/firstrunlogsvc.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # 运行 logproxy
-nohup ./logproxy > proxy.log 2>&1 &
-
-# 运行 logtail
-nohup ./tail --log_file /work/CloudBox/logsvc/Bin/screen.log --log_app scrsvc --log_type gostd --log_seek 2 > tail.log 2>&1 &
-
-# 运行 logsearcher
-nohup ./logsearcher --searcher_es_domain="http://192.168.3.26:9200" > searcher.log 2>&1 &
+nohup ./logproxy &
 
 # 运行 loganalysis
-nohup ./loganalysis --analysis_es_domain="http://192.168.3.26:9200" > analysis.log 2>&1 &
+nohup ./loganalysis --analysis_es_domain="http://172.31.235.243:9200" --analysis_consumer_domain="tcp://172.31.235.243:29000" &
+
+# 运行 logsearcher
+nohup ./logsearcher --searcher_es_domain="http://172.31.235.243:9200" --searcher_consumer_domain="tcp://172.31.235.243:29000" &
+
+# 运行 logtail
+nohup ./logtail --log_file /opt/data/log/cloudbox/scrsvc.log --log_app scrsvc --log_type beego --log_seek 0 &
